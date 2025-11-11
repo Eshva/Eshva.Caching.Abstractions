3491435c5e71ec4ac916e6727b30c6b4a14cf448
Mike Eshva <reg@eshva.ru>
2025-11-11 16:44:59 +0300

    Remove statistics property providing which could slow down cache invalidation

--- code/solutions/Eshva.Caching.Abstractions/CacheInvalidationStatistics.cs
+++ code/solutions/Eshva.Caching.Abstractions/CacheInvalidationStatistics.cs
@@ -5,7 +5,6 @@ namespace Eshva.Caching.Abstractions;
 /// <summary>
 /// Cache invalidation statistics.
 /// </summary>
-/// <param name="TotalEntriesCount">Total cache entries scanned.</param>
 /// <param name="PurgedEntriesCount">Cache entries purged.</param>
 [PublicAPI]
-public readonly record struct CacheInvalidationStatistics(uint TotalEntriesCount, uint PurgedEntriesCount);
+public readonly record struct CacheInvalidationStatistics(uint PurgedEntriesCount);
